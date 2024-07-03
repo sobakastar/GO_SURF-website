588408d1e641179f4596205fcfe7c35c64821641
sobakastar <dr.gris@yandex.ru>
2024-07-03 21:58:36 +0300

    initialization of the project after half of the layout

---
+++ README.md
@@ -0,0 +1,14 @@
+#  Верстке с использованием сборки на gulp 4 и scss, использован Slick.JS
+#   Макет  figma, присутствуют интерактивные элементы. 
+
+#  Layout using assembly on gulp 4 and scs, used Slick.JS
+# Figma layout, interactive elements are present.
+
+
+##  [ссылка на макет/link to the layout](https://www.figma.com/design/yqTfu0VqosY86wml4VdBsL/gosurf-blackflagcreative?node-id=0-1&t=3k2p1jikGGCcaeYW-0)
+
+## запуск gulp/launch gulp: 
+## npm i gulp@4.0.2
+## gulp
+ 
+![demo](./demo-bg.png)
---
+++ app/js/main.js
@@ -0,0 +1,33 @@
+$(function() {
+  $('.header__slider').slick({
+    infinite: true,
+    fade: true,
+    prevArrow: '<img class="slider-arrows slider-arrow_left" src="images/arrow-left.svg" alt="">',
+    nextArrow: '<img class="slider-arrows slider-arrow_right" src="images/arrow-right.svg" alt="">',
+    asNavFor: '.slider-dots'
+  });
+
+  $('.slider-dots').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: '.header__slider',
+  });
+
+  $('.surf-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<img class="slider-arrows slider-arrow_left" src="images/arrow-left.svg" alt="">',
+    nextArrow: '<img class="slider-arrows slider-arrow_right" src="images/arrow-right.svg" alt="">',
+    asNavFor: '.slider-map',
+  })
+
+  $('.slider-map').slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.surf-slider',
+    focusOnSelect: true,
+  })
+
+}) 
+
